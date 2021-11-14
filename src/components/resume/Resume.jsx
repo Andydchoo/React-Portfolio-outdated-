3ebd7d5dd55f2e9499dba0138e0c4430baeee95f
Andrew Choo <andydchoo@gmail.com>
2021-11-14 18:47:09 -0500

    added resume sloppily

--- src/components/resume/Resume.jsx
+++ src/components/resume/Resume.jsx
@@ -3,7 +3,7 @@ import './resume.css'
 export default function Resume() {
     return (
         <div className="resume" id="resume">
-            
+            <a href="https://docs.google.com/document/d/1WmBt5b2q4WbkGnadHP-Qx6uI7-VqkDmVg1RgikmBp2k/edit?usp=sharing">Resume</a>
         </div>
     )
 }
