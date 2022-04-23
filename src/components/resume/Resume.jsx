77ce59001ff8685f2100589df2665d5939fc2b89
Andy Choo <andydchoo@gmail.com>
2022-04-23 11:40:24 -1000

    changes to resume. removed unused vars

--- src/components/resume/Resume.jsx
+++ src/components/resume/Resume.jsx
@@ -1,7 +1,7 @@
 //Align and style pdf to be more aesthetic
 //fix design of left side
 import './resume.css'
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import Stack from '@mui/material/Stack';
 import { Document, Page, pdfjs } from 'react-pdf';
 import { View } from '@react-pdf/renderer';
@@ -16,10 +16,6 @@ const options = {
 export default function Resume() {
     const [file, setFile] = useState('assets/Resume.pdf');
     const [numPages, setNumPages] = useState(null);
-
-    function onFileChange(event) {
-        setFile(event.target.files[0]);
-    }
 
     function onDocumentLoadSuccess({ numPages: nextNumPages }) {
         setNumPages(nextNumPages);
@@ -50,38 +46,20 @@ export default function Resume() {
                 <a 
                   href="https://docs.google.com/document/d/1WmBt5b2q4WbkGnadHP-Qx6uI7-VqkDmVg1RgikmBp2k/edit?usp=sharing"
                   style={{
-                      textDecoration: 'none'
+                      textDecoration: 'underline'
                   }}
                 >
                     Resume
                 </a>
             </h1>
-            <Document file={file} onLoadSuccess={onDocumentLoadSuccess} options={options} style={{flex: '1', maxHeight: '100%'}}>
+            <Document file={file} onLoadSuccess={onDocumentLoadSuccess} options={options} style={{display: 'flex', flex: '1', height: '70%'}}>
                 {Array.from(new Array(numPages), (el, index) => (
-                    <Page key={`page_${index + 1}`} pageNumber={index + 1} style={{height: '70%'}}>
-                      <View style={{ color: 'white', textAlign: 'center', margin: 30, Maxheight: '70%'}}>
-
+                    <Page size="A4" key={`page_${index + 1}`} pageNumber={index + 1} style={{}}>
+                      <View style={{ color: 'white', textAlign: 'center', margin: 30, Maxheight: '70%', overflow: 'hidden'}}>
                       </View>
                     </Page>
                 ))}
             </Document>
-            {/* <a
-                href='.resume.pdf'
-                target="_blank"
-                style={{
-                    display: 'flex',
-                    justifyContent: 'center',
-                    overflow: 'hidden',
-                    height: '75vh',
-                    paddingRight: '5px',
-                    paddingLeft: '5px'
-                    }}
-                >
-                <img
-                    src="assets/resume.jpg"
-                    alt="Resume"
-                />
-            </a> */}
         </div>
     )
 }
