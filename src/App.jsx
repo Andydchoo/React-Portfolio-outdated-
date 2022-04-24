bfae8696d6bf55dc6e781be178fda5483c53bbe3
Andy Choo <andydchoo@gmail.com>
2022-04-23 14:47:32 -1000

    commented contact out

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Navbar from "./components/navbar/Navbar";
 import Intro from "./components/intro/Intro";
 import Portfolio from "./components/portfolio/Portfolio";
 import Resume from "./components/resume/Resume";
-import Contact from "./components/contact/Contact";
+// import Contact from "./components/contact/Contact";
 import Footer from "./components/footer/Footer";
 import "./app.css";
 
