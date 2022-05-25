279b69e2b72419cc5d10add549518fd4fa47b7f2
Andy Choo <andydchoo@gmail.com>
2022-05-25 17:52:46 -0400

    fixing resume

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,21 @@
 import Navbar from "./components/navbar/Navbar";
 import Intro from "./components/intro/Intro";
 import Portfolio from "./components/portfolio/Portfolio";
-import Resume from "./components/resume/Resume";
+// import Resume from "./components/resume/Resume";
 // import Contact from "./components/contact/Contact";
 import Footer from "./components/footer/Footer";
 import "./app.css";
 
-function App() {
+export default function App() {
   return (
       <div className="app">
         <Navbar />
         <div className="sections">
           <Intro />
           <Portfolio />
-          <Resume />
+          {/* <Resume /> */}
           <Footer />
         </div>
       </div>
   );
 }
-
-export default App;
